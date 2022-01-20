00ecc41cc95f8b06a873d1a13907ae17329e2950
Onuchukwu Chika Augustine <chuzksy@gmail.com>
2022-01-20 04:55:46 +0100

    Implement LogManager for logging using NLog

--- CompanyEmployees/CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers
@@ -8,12 +9,12 @@ namespace CompanyEmployees.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILoggerManager _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILoggerManager logger)
         {
             _logger = logger;
         }
@@ -21,6 +22,11 @@ namespace CompanyEmployees.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInfo("Here is info message from our values controller."); 
+            _logger.LogDebug("Here is debug message from our values controller."); 
+            _logger.LogWarn("Here is warn message from our values controller."); 
+            _logger.LogError("Here is an error message from our values controller.");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
